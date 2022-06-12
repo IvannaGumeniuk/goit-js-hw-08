4d57de0c70fa5b3b7082915226afc0f63de31a93
IvannaGumeniuk <vany93ok@gmail.com>
2022-06-12 17:28:13 +0300

    hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const player = new Player(iframe);
+let time = localStorage.getItem('LOCALSTORAGE_KEY')
+player
+   .setCurrentTime(Number(time))
+   .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+   })
+   .catch(function (error) {
+      switch (error.name) {
+         case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+         default:
+            // some other error occurred
+            break;
+      }
+   });
+player.on('timeupdate', throttle(getSecondTime, 1000));
+
+function getSecondTime(data) {
+   localStorage.setItem('LOCALSTORAGE_KEY', data.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+const form = document.querySelector(".feedback - form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
+form.addEventListener("input",);
+
+localStorage.setItem("email", "");
+localStorage.setItem("message", "");
