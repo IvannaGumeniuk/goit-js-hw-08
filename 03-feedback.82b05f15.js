88489b501646c3891d23c7e0f7610bb6cfc0d210
IvannaGumeniuk <93284615+IvannaGumeniuk@users.noreply.github.com>
2022-06-12 15:52:51 +0000

    Deploying to gh-pages from @ IvannaGumeniuk/goit-js-hw-08@85f2d84b3fb85fa1bffda2c9b52f96f5f1e3016d 🚀

--- 03-feedback.82b05f15.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback - form").addEventListener("input"),localStorage.setItem("email",""),localStorage.setItem("message","");
-//# sourceMappingURL=03-feedback.82b05f15.js.map
--- 03-feedback.914d1291.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback - form").addEventListener("input"),localStorage.setItem("email",""),localStorage.setItem("message","");
-//# sourceMappingURL=03-feedback.914d1291.js.map
